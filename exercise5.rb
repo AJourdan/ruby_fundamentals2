39f6f3a608df56f464aee3e57b9f4b7721a07d89
Alon Jourdan <alon.jourdan@gmail.com>
2015-10-28 14:13:30 -0400

    Finish exercise 5

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,8 @@
-print "Give me a temperature in Farenheit"
+print "Give me a temperature in Farenheit and we will convert it to Celcius"
 temp = gets.chomp
 
 def num(temp)
 	(temp.to_i - 32) * 5/9
 end
+
+puts "#{temp} Farenheit is equal to #{num(temp)} in Celcius "
