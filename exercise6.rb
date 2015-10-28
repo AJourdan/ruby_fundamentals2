a0958b728212cd836520bf2c5de20763ce1e36b2
Alon Jourdan <alon.jourdan@gmail.com>
2015-10-28 16:25:54 -0400

    exercise6.rb

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,38 @@
-groceries = ["carrots", "toilet paper", "apples", "salmon"]
+groceries = ["carrots", "toilet paper", "apples", "salmon",]
 
-groceries.each do |grocery|
-  puts "* #{grocery}"	
+groceries << "rice"
+
+def print_list(list)
+	list.each do |grocery|
+  		puts "* #{grocery}"	
+	end
 end
+
+
+
+#count how many items there are
+
+puts groceries.count
+
+#Check for Bananas
+
+if groceries.include?("bananas")
+	puts "You don't need to pick up bananas today"
+else
+	puts "You need to pick up bananas"
+end
+
+
+#Display second item in list
+
+puts groceries[1]
+
+#Store alphabetically
+
+print_list(groceries.sort)
+
+
+#delete salmon from array & display
+
+groceries.delete("salmon")
+print_list(groceries)
